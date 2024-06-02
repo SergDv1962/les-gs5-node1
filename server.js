aff86eacefd2b527ad166d1ee15ba1bee6050d1c
unknown <dsv19621227@gmail.com>
2024-06-02 20:51:17 +0300

    create server

---
+++ server.js
@@ -0,0 +1,56 @@
+const http = require('http');
+
+const host = '127.0.0.1';
+const port = 7000;
+
+function notFound(res) {
+   res.statusCode = 404;
+   res.setHeader('Content-Type', 'text/plain');
+   res.end('Not Found\nError 404')
+};
+
+const server = http.createServer((req, res) => {
+   switch (req.method) {
+      case 'GET':
+         switch (req.url) {
+            case '/home':
+               res.statusCode = 200;
+               res.setHeader('Content-Type', 'text/plain');
+               res.end('Home page\n');
+               break;
+            case '/about':
+               res.statusCode = 200;
+               res.setHeader('Content-Type', 'text/plain');
+               res.end('About page\n');
+               break;
+            default:
+               notFound(res);
+               break;
+         }
+         break;
+      case 'POST':
+         switch (req.url) {
+            case '/api/admin':
+               res.statusCode = 200;
+               res.setHeader('Content-Type', 'text/plain');
+               res.end('Create admin request\n');
+               break;
+            case '/api/user':
+               res.statusCode = 200;
+               res.setHeader('Content-Type', 'text/plain');
+               res.end('Creat user request\n');
+               break;
+            default:
+               notFound(res);
+               break;
+         }
+         break;
+      default:
+         notFound(res);
+         break;
+   }
+});
+
+server.listen(port, host, () => {
+   console.log(`Server listening http://${host}:${port}`)
+})
